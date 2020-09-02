1113dc854b950ea2754af199820f529249f9e0f6
Timur Kim <allrighty1809@gmail.com>
2020-09-02 20:24:38 +0600

    Added alt. translation and definition of word

--- lib/src/google_translator.dart
+++ lib/src/google_translator.dart
@@ -69,12 +69,112 @@ class GoogleTranslator {
     }
 
     final translated = sb.toString();
+    final alternateTranslated = await _getAltTranslation(sourceText, from, to);
+    final definition = await _getDefinition(sourceText, from, to);
     return _Translation(
       translated,
+      alternateTranslated,
+      definition,
       source: sourceText,
       sourceLanguage: _languageList[from],
       targetLanguage: _languageList[to],
     );
+  }
+
+  // Get alt. translation
+  Future<List<String>> _getAltTranslation(
+    String sourceText,
+    String from,
+    String to,
+  ) async {
+    final parameters = {
+      'client': client == ClientType.siteGT ? 't' : 'gtx',
+      'sl': from,
+      'tl': to,
+      'hl': to,
+      'dt': 'at',
+      'ie': 'UTF-8',
+      'oe': 'UTF-8',
+      'otf': '1',
+      'ssel': '0',
+      'tsel': '0',
+      'kc': '7',
+      'tk': _tokenProvider.generateToken(sourceText),
+      'q': sourceText
+    };
+
+    var url = Uri.https(_baseUrl, _path, parameters);
+    final data = await http.get(url);
+
+    if (data.statusCode != 200) {
+      throw http.ClientException('Error ${data.statusCode}: ${data.body}', url);
+    }
+
+    final jsonData = jsonDecode(data.body);
+    if (jsonData[5] != null) {
+      final List<String> tList = [];
+
+      final List correctData = jsonData[5][0][2];
+
+      for (int i = 0; i < correctData.length; i++) {
+        tList.add(jsonData[5][0][2][i][0]);
+      }
+      return tList;
+    } else {
+      return null;
+    }
+  }
+
+  // Get definition of text
+  Future<Map<String, List<String>>> _getDefinition(
+    String sourceText,
+    String from,
+    String to,
+  ) async {
+    final parameters = {
+      'client': client == ClientType.siteGT ? 't' : 'gtx',
+      'sl': from,
+      'tl': to,
+      'hl': to,
+      'dt': 'md',
+      'ie': 'UTF-8',
+      'oe': 'UTF-8',
+      'otf': '1',
+      'ssel': '0',
+      'tsel': '0',
+      'kc': '7',
+      'tk': _tokenProvider.generateToken(sourceText),
+      'q': sourceText
+    };
+
+    var url = Uri.https(_baseUrl, _path, parameters);
+    final data = await http.get(url);
+
+    if (data.statusCode != 200) {
+      throw http.ClientException('Error ${data.statusCode}: ${data.body}', url);
+    }
+
+    final jsonData = jsonDecode(data.body);
+
+    Map<String, List<String>> definitionsMap = {};
+    if (jsonData.length < 12) {
+      return null;
+    } else {
+      if (jsonData[12] != null) {
+        final List list = jsonData[12];
+        for (int i = 0; i < list.length; i++) {
+          String title = jsonData[12][i][0];
+          List<String> definitions = [];
+          List inList = jsonData[12][i][1];
+          for (int n = 0; n < inList.length; n++)
+            definitions.add(jsonData[12][i][1][n][0]);
+          definitionsMap[title] = definitions;
+        }
+        return definitionsMap;
+      } else {
+        return null;
+      }
+    }
   }
 
   /// Translates and prints directly
--- lib/src/model/translation.dart
+++ lib/src/model/translation.dart
@@ -3,12 +3,20 @@ part of google_transl;
 /// Translation returned from GoogleTranslator.translate method, containing the translated text, the source text, the translated language and the source language
 abstract class Translation {
   final String text;
+  final List<String> alternative;
+  final Map<String, List<String>> definition;
   final String source;
   final Language targetLanguage;
   final Language sourceLanguage;
 
   Translation._(
-      this.text, this.source, this.sourceLanguage, this.targetLanguage);
+    this.text,
+    this.source,
+    this.sourceLanguage,
+    this.targetLanguage,
+    this.alternative,
+    this.definition,
+  );
 
   String operator +(other);
 
@@ -18,16 +26,27 @@ abstract class Translation {
 
 class _Translation extends Translation {
   final String text;
+  final List<String> alternative;
+  final Map<String, List<String>> definition;
   final String source;
   final Language sourceLanguage;
   final Language targetLanguage;
 
   _Translation(
-    this.text, {
+    this.text,
+    this.alternative,
+    this.definition, {
     this.sourceLanguage,
     this.targetLanguage,
     this.source,
-  }) : super._(text, source, sourceLanguage, targetLanguage);
+  }) : super._(
+          text,
+          source,
+          sourceLanguage,
+          targetLanguage,
+          alternative,
+          definition,
+        );
 
   String operator +(other) => this.toString() + other.toString();
 }
